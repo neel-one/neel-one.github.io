2ae2572f06d4f962128703596e84d27bbafbcecb
neelsh <neelsh@umich.edu>
2021-08-25 14:39:57 -0400

    fix deploy

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,5 @@
-mv googleddd737c5f7b3000e.html tmp/
+mkdir tmp
+mv googleddd737c5f7b3000.html tmp/
 rm -r admin/ authors/ categories/ css/ en/ event/ js/ media/ post/ project/ publication_types/ slides/ tags/ talk/ uploads/
 rm _headers _redirects *.html *.json *.xml *.txt index.webmanifest
 rm -rf public/
